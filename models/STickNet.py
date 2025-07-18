72139f311e0e70ccb0f867e4aedd7e7285ed66ba
Ngoc Hoang <ngoc.hc91@gmail.com>
2025-07-18 22:30:13 +0700

    Refactor STickNet.py to improve code readability and structure. Remove SPATIAL_TICKNET_CHANNEL_OPTIONS and integrate channel configurations directly in build_STickNet function. Enhance formatting and consistency across the codebase.

--- models/STickNet.py
+++ models/STickNet.py
@@ -1,16 +1,8 @@
 import torch
 import torch.nn as nn
-import torch.nn
 
 from .SE_Attention import *
 from .common import Classifier, conv1x1_block, conv3x3_block, conv3x3_dw_blockAll
-
-SPATIAL_TICKNET_CHANNEL_OPTIONS = {
-  'basic': [[256], [128, 64], [128], [256], [512]],
-  'small': [[256], [128, 64, 128], [256, 512, 256,128], [64, 128, 256], [512]],
-  'large': [[256,128], [64,128,256],[512,256, 128, 64, 128, 256, 512], [ 256, 128, 64, 128, 256], [512]],
-}
-
 
 class FR_PDP_block(torch.nn.Module):
   """
@@ -18,21 +10,16 @@ class FR_PDP_block(torch.nn.Module):
   """
 
   def __init__(self,
-               in_channels,
-               out_channels,
-               stride):
+    in_channels,
+    out_channels,
+    stride):
+
     super().__init__()
-    self.Pw1 = conv1x1_block(in_channels=in_channels,
-                             out_channels=in_channels,
-                             use_bn=False,
-                             activation=None)
+    self.Pw1 = conv1x1_block(in_channels=in_channels, out_channels=in_channels, use_bn=False, activation=None)
     self.Dw = conv3x3_dw_blockAll(channels=in_channels, stride=stride)
-    self.Pw2 = conv1x1_block(in_channels=in_channels,
-                             out_channels=out_channels,
-                             groups=1)
-    self.PwR = conv1x1_block(in_channels=in_channels,
-                             out_channels=out_channels,
-                             stride=stride)
+    self.Pw2 = conv1x1_block(in_channels=in_channels, out_channels=out_channels,groups=1)
+    self.PwR = conv1x1_block(in_channels=in_channels, out_channels=out_channels,stride=stride)
+
     self.stride = stride
     self.in_channels = in_channels
     self.out_channels = out_channels
@@ -57,53 +44,60 @@ class SpatialTickNet(nn.Module):
   Class for constructing SpatialTickNet, enhancing TickNet for spatial feature learning.
   """
   def __init__(self,
-               num_classes,
-               init_conv_channels,
-               init_conv_stride,
-               channels,
-               strides,
-               in_channels=3,
-               in_size=(224, 224),
-               use_data_batchnorm=True
-               ):
-
+    num_classes,
+    init_conv_channels,
+    init_conv_stride,
+    channels,
+    strides,
+    in_channels=3,
+    in_size=(224, 224),
+    use_data_batchnorm=True
+  ):
+    
     super().__init__()
     self.use_data_batchnorm = use_data_batchnorm
     self.in_size = in_size
 
     self.backbone = torch.nn.Sequential()
 
-   # data batchnorm
+    # data batchnorm
     if self.use_data_batchnorm:
-      self.backbone.add_module(
-          "data_bn", torch.nn.BatchNorm2d(num_features=in_channels))
+      self.backbone.add_module("data_bn", torch.nn.BatchNorm2d(num_features=in_channels))
 
     # init conv
-    self.backbone.add_module("init_conv", conv3x3_block(
-        in_channels=in_channels, out_channels=init_conv_channels, stride=init_conv_stride))
+    self.backbone.add_module("init_conv", conv3x3_block(in_channels=in_channels, out_channels=init_conv_channels,stride=init_conv_stride))
 
     # stages
     in_channels = init_conv_channels
-    in_channels = self.add_stages(in_channels, channels, strides)
+    for stage_id, stage_channels in enumerate(channels):
+      stage = torch.nn.Sequential()
+      for unit_id, unit_channels in enumerate(stage_channels):
+        stride = strides[stage_id] if unit_id == 0 else 1
+        stage.add_module("unit{}".format(unit_id + 1),
+          FR_PDP_block(in_channels=in_channels, out_channels=unit_channels, stride=stride))
+        in_channels = unit_channels
+      self.backbone.add_module("stage{}".format(stage_id + 1), stage)
     self.final_conv_channels = 1024
-
     self.backbone.add_module("final_conv", conv1x1_block(in_channels=in_channels, out_channels=self.final_conv_channels, activation="relu"))
-    self.backbone.add_module("dropout1",torch.nn.Dropout2d(0.2))#with dropout
+    self.backbone.add_module("dropout1", torch.nn.Dropout2d(0.2)) # with dropout
     self.backbone.add_module("global_pool", torch.nn.AdaptiveAvgPool2d(output_size=1))
-    self.backbone.add_module("dropout2",torch.nn.Dropout2d(0.2))#with dropout
+    self.backbone.add_module("dropout2", torch.nn.Dropout2d(0.2)) # with dropout
     in_channels = self.final_conv_channels
     # classifier
-    self.classifier = Classifier(in_channels=in_channels, num_classes=num_classes)
+    self.classifier = Classifier(
+      in_channels=in_channels,
+      num_classes=num_classes)
 
     self.init_params()
 
   def init_params(self):
+    # backbone
     for _, module in self.backbone.named_modules():
-      if isinstance(module, nn.Conv2d):
-        nn.init.kaiming_uniform_(module.weight)
+      if isinstance(module, torch.nn.Conv2d):
+        torch.nn.init.kaiming_uniform_(module.weight)
         if module.bias is not None:
-          nn.init.constant_(module.bias, 0)
-
+          torch.nn.init.constant_(module.bias, 0)
+    # classifier
     self.classifier.init_params()
 
   def forward(self, x):
@@ -111,25 +105,18 @@ class SpatialTickNet(nn.Module):
     x = self.classifier(x)
     return x
 
-  def add_stages(self, in_channels, channels, strides):
-    for stage_id, stage_channels in enumerate(channels):
-      stage = nn.Sequential()
-      for unit_id, unit_channels in enumerate(stage_channels):
-        stride = strides[stage_id] if unit_id == 0 else 1
-
-        stage.add_module(
-            "unit{}".format(unit_id + 1),
-            FR_PDP_block(in_channels=in_channels,
-                         out_channels=unit_channels, stride=stride)
-        )
-        # print(f'add_stages: stage({stage_id + 1}), node({unit_id + 1}), stride({stride})')
-        in_channels = unit_channels
-      self.backbone.add_module("stage{}".format(stage_id + 1), stage)
-    return in_channels
-
-def build_SpatialTickNet(num_classes, typesize='basic', cifar=False):
+###
+#%% model definitions
+###
+def build_STickNet(num_classes, typesize='small', cifar=False):
   init_conv_channels = 32
-  channels = SPATIAL_TICKNET_CHANNEL_OPTIONS.get(typesize)
+  if typesize == 'basic':
+    channels = [[256], [128, 64], [128], [256], [512]]
+  if typesize == 'small':
+    channels = [[256], [128, 64, 128], [256, 512, 256, 128], [64, 128, 256], [512]]
+  if typesize == 'large':
+    channels = [[256, 128], [64, 128, 256], [512, 256, 128, 64, 128, 256, 512], [256, 128, 64, 128, 256], [512]]
+
   if cifar:
     in_size = (32, 32)
     init_conv_stride = 1
@@ -140,10 +127,9 @@ def build_SpatialTickNet(num_classes, typesize='basic', cifar=False):
     strides = [2, 1, 2, 2, 2] # for all
 
   return SpatialTickNet(
-      num_classes=num_classes,
-      init_conv_channels=init_conv_channels,
-      init_conv_stride=init_conv_stride,
-      channels=channels,
-      strides=strides,
-      in_size=in_size
-  )
+    num_classes=num_classes,
+    init_conv_channels=init_conv_channels,
+    init_conv_stride=init_conv_stride,
+    channels=channels,
+    strides=strides,
+    in_size=in_size)
