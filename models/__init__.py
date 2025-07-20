4ef359f2a430a93866759a0786d63e11df8c0354
Ngoc Hoang <ngoc.hc91@gmail.com>
2025-07-20 23:48:11 +0700

    Add __init__.py file for models package to define module exports and improve package structure

---
+++ models/__init__.py
@@ -0,0 +1,18 @@
+# Models package for STickNet project
+
+from .STickNet import build_STickNet, SpatialTickNet, FR_PDP_block
+from .common import Classifier, conv1x1_block, conv3x3_block, conv3x3_dw_blockAll, LWO
+from .SE_Attention import SE
+from .datasets import *
+
+__all__ = [
+  'build_STickNet',
+  'SpatialTickNet', 
+  'FR_PDP_block',
+  'Classifier',
+  'conv1x1_block',
+  'conv3x3_block', 
+  'conv3x3_dw_blockAll',
+  'LWO',
+  'SE'
+]
